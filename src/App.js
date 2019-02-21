b423b6efaaa5233611900c60b3722337613b1b12
DomaLu <kaku18ccf52@gmail.com>
2019-02-22 05:36:21 +0800

    setup react lazy, Suspense apis

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react'
+import React, { lazy, Component, Suspense } from 'react'
 import { hot } from 'react-hot-loader'
+
+const Warning = lazy(() => import('./Warning'))
 
 class App extends Component {
   state = {
     count: 0
   }
-
   increment = () => {
     this.setState(state => ({ count: state.count + 1 }))
   }
-
   decrement = () => {
     this.setState(state => ({ count: state.count - 1 }))
   }
-
   render() {
     const { count } = this.state
     return (
@@ -22,6 +21,7 @@ class App extends Component {
         <h2 className={count > 10 ? 'warning' : null}>Count: {count}</h2>
         <button onClick={this.increment}>+</button>
         <button onClick={this.decrement}>-</button>
+        {count > 10 ? <Suspense fallback={null}>{<Warning />}</Suspense> : null}
       </div>
     )
   }
