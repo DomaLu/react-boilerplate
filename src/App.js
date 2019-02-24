584aa7fe7ea6cc16e599c88e240f126411f037b7
DomaLu <kaku18ccf52@gmail.com>
2019-02-25 00:51:59 +0800

    init clean up

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
-import React, { lazy, Component, Suspense } from 'react'
+import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
 
-const Warning = lazy(() => import('./Warning'))
-
 class App extends Component {
-  state = {
-    count: 0
-  }
-  increment = () => {
-    this.setState(state => ({ count: state.count + 1 }))
-  }
-  decrement = () => {
-    this.setState(state => ({ count: state.count - 1 }))
-  }
   render() {
-    // throw new Error('Boom!')
-    const { count } = this.state
-    return (
-      <div>
-        <h1>Hello World</h1>
-        <h2 className={count > 10 ? 'warning' : null}>Count: {count}</h2>
-        <button onClick={this.increment}>+</button>
-        <button onClick={this.decrement}>-</button>
-        {count > 10 ? <Suspense fallback={null}>{<Warning />}</Suspense> : null}
-      </div>
-    )
+    return <h1>Hello World</h1>
   }
 }
-
-// const hotFunction = hot(module)
-// export default hotFunction(App)
 export default hot(module)(App)
